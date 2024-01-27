b4734bbe36781b26de5ce69bf01d5a03313eee74
Lemon Proust <valdemar.buco@gmail.com>
2024-01-27 17:56:31 +0000

    ActorDTO e UserRepository

--- Locadora/src/main/java/ulht/doa/repositories/UserRepository.java
+++ Locadora/src/main/java/ulht/doa/repositories/UserRepository.java
@@ -1,9 +1,33 @@
 package ulht.doa.repositories;
 
+import io.micronaut.data.annotation.Query;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
+import io.micronaut.context.annotation.Parameter;
 import ulht.doa.entities.UserEntity;
+
+import java.util.List;
+
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    // Find users by id
+    @Query("SELECT a FROM UserEntity a WHERE a.id = :id")
+    List<UserEntity> findUsersById(Long id);
+
+    // Find users by name
+    @Query("SELECT a FROM UserEntity a WHERE a.name = :name")
+    List<UserEntity> findUsersByName(String name);
+
+    // Find user whose name starts with a
+    @Query("SELECT a FROM UserEntity a WHERE LOWER(a.name) LIKE 'a%'")
+    List<UserEntity> findUsersWhoseNamesStartWithA();
+
+    @Query("SELECT a FROM UserEntity a WHERE a.email = :email")
+    List<UserEntity> findUsersByEmail(String email);
+
+//    @Query("SELECT a FROM UserEntity a WHERE size(a.books) > :minBookCount")
+//    List<UserEntity> findAuthorsWithMinBooks(@Parameter("minBookCount") int minBookCount);
+
 
 }
